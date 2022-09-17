d79b4f57ea69fa40cbe008ac04d3dd53b7d68b90
Khoi Duong <56575903+duonghuukhoi@users.noreply.github.com>
2022-09-17 21:07:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "3.82.235.91",
+                  "18.206.55.138",
                   "db_admin",
                   "rmit_password",
                   "rmit_store_db"
